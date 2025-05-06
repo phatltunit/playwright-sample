142a33dd35458a42d2855800a12b0bad43e0d5c2
PhatLT <phatlt@unit.com.vn>
2025-05-06 11:46:15 +0700

    Update test casess

--- tests/test-1.public.ts
+++ tests/test-1.public.ts
@@ -2,7 +2,8 @@ import {expect, test } from '@playwright/test';
 import { BASE_URL } from '../env';
 
 
-test('Test without authenticated', async ({ page }) => {
+// if the user does not login, the page should show the login button
+test('The user should see the Login button', async ({ page }) => {
     await page.goto(BASE_URL);
-    await expect(page.locator('button')).toContainText('Login');
+    await expect(page.locator('button:has-text("Login")')).toBeVisible();
 });
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,26 +1,22 @@
-import {expect } from '@playwright/test';
-import { base as test } from './base/base';
+import { expect } from "@playwright/test";
+import { base as test } from "./base/base";
+import { BASE_URL } from "../env";
 
+test("Test run ad bonus show message run report success", async ({ page }) => {
+  await page.getByRole("link", { name: " COMPENSATION SETUP " }).click();
+  await page.getByRole("link", { name: " Compensation Setup " }).click();
+  await page.getByRole("link", { name: " C&B Setup" }).click();
+  await page.locator("#select2-agentType-container").click();
+  await page.getByRole("treeitem", { name: "AD - Area Manager" }).click();
+  await page.getByText("×AD - Area Manager").click();
+  await page.getByRole("treeitem", { name: "FM - Financial Manager" }).click();
+  await page.getByRole("link", { name: " Scheme Management" }).click();
+  await page.getByRole("link", { name: "" }).first().click();
+  await page.locator("#linkList").click();
+  await expect(page.locator("h1")).toContainText("Scheme Management");
+});
 
-
-
-
-test('Test run ad bonus show message run report success', async ({ page }) => {
-  await page.getByRole('link', { name: ' COMPENSATION SETUP ' }).click();
-  await page.getByRole('link', { name: ' Compensation Setup ' }).click();
-  await page.getByRole('link', { name: ' C&B Setup' }).click();
-  await page.locator('#select2-agentType-container').click();
-  await page.getByRole('treeitem', { name: 'AD - Area Manager' }).click();
-  await page.getByText('×AD - Area Manager').click();
-  await page.getByRole('treeitem', { name: 'FM - Financial Manager' }).click();
-  await page.getByRole('link', { name: ' Scheme Management' }).click();
-  await page.getByRole('link', { name: '' }).first().click();
-  await page.locator('#linkList').click();
-  await expect(page.locator('h1')).toContainText('Scheme Management');
-  });
-  
-  
-  test('Test with authenticated', async ({ page }) => {
-  
-  
-  });
+test("The user should not see the login button", async ({ page }) => {
+  await page.goto(BASE_URL);
+  await expect(page.locator('button:has-text("Login")')).not.toBeVisible();
+});
