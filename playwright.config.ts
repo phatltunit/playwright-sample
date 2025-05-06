21d5f70dc636b09c0fb4bc075f7e6e7dfe3dcd4d
PhatLT <phatlt@unit.com.vn>
2025-05-06 11:33:57 +0700

    Update some stuffs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,71 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
+const browsers = [
+  {
+    name: "chromium",
+    device: "Desktop Chrome",
+  },
+  {
+    name: "firefox",
+    device: "Desktop Firefox",
+  },
+  {
+    name: "edge",
+    device: "Microsoft Edge",
+    channel: "msedge",
+  },
+];
+
+const configuration = [
+  {
+    suffix: "",
+    testMatch: /.*\.spec\.ts/,
+    storageState: "playwright/.auth/user.json",
+  },
+  { suffix: "-non-auth", testMatch: /.*\.public\.ts/ },
+];
+
+interface Project {
+  name: string;
+  use?: {
+    [key: string]: any;
+    storageState?: string;
+    channel?: string;
+  };
+  dependencies?: string[];
+  testMatch?: RegExp;
+}
+
+const projects: Project[] = [];
+const setupProject: Project = {
+  name: "setup",
+  testMatch: /.*\.setup\.ts/,
+};
+// Pre-authentication project
+projects.push(setupProject);
+
+// Loop through the configuration and browsers to create projects
+configuration.forEach((config) => {
+  browsers.forEach((browser) => {
+    const project = {
+      name: `${browser.name}${config.suffix}`,
+      use: {
+        ...devices[browser.device],
+        storageState: config.storageState,
+        channel: browser.channel,
+      },
+      testMatch: config.testMatch,
+      dependencies: ["setup"],
+    };
+    projects.push(project);
+  });
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -15,87 +75,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {name: 'setup', testMatch: /.*\.setup\.ts/},
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'], storageState: 'playwright/.auth/user.json' },
-      dependencies: ['setup'],
-      testMatch: /.*\.spec\.ts/,
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'], storageState: 'playwright/.auth/user.json' },
-      dependencies: ['setup'],
-      testMatch: /.*\.spec\.ts/,
-    },
-
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge', storageState: 'playwright/.auth/user.json' },
-      dependencies: ['setup'],
-      testMatch: /.*\.spec\.ts/,
-    },
-    
-    // without authentication
-
-    {
-      name: 'chromium-non-auth',
-      use: { ...devices['Desktop Chrome'], storageState: undefined},
-      dependencies: ['setup'],
-      testMatch: /.*\.public\.ts/,
-    },
-
-    {
-      name: 'firefox-non-auth',
-      use: { ...devices['Desktop Firefox'], storageState: undefined },
-      dependencies: ['setup'],
-      testMatch: /.*\.public\.ts/,
-    },
-
-    {
-      name: 'Microsoft Edge-non-auth',
-      use: { ...devices['Desktop Edge'], channel: 'msedge', storageState: undefined },
-      dependencies: ['setup'],
-      testMatch: /.*\.public\.ts/,
-    },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-   
-
-    
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+  projects: projects,
 
   /* Run your local dev server before starting the tests */
   // webServer: {
