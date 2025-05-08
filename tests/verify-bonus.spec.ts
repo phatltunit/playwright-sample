b5b6316ec314ff64801da0b3506a52a97f195e53
PhatLT <phatlt@unit.com.vn>
2025-05-08 13:56:27 +0700

    Refactor bonus report tests to use a helper function for response handling

--- tests/verify-bonus.spec.ts
+++ tests/verify-bonus.spec.ts
@@ -1,17 +1,19 @@
-import { expect, test } from "@playwright/test";
+import { expect, test, Page } from "@playwright/test";
 import { testResponse } from "../utils/responseUtil";
 import { BASE_URL } from "../env";
 import { bonusUrl } from "../utils/bonus-urls";
 
 
 
-
+const waitForResponse = async (page: Page) => {
+    await testResponse(page, "**/ajaxList");
+    await expect(page.locator("div.isloading-overlay")).not.toBeVisible();
+};
 
 bonusUrl.forEach((url) => {
     test(`Test ${url} show report`, async ({ page }) => {
         await page.goto(`${BASE_URL}/bonus-report/${url}/list`);
         await page.locator("#btnSearch").click();
-        await testResponse(page, "**/ajaxList");
-        await expect(page.locator("div.isloading-overlay")).not.toBeVisible();
+        await waitForResponse(page);
     });
 });
